97d191447d56a3e3712181e3bf338360c76f47ec
Cesar Martinez <cesar_a_mar_@hotmail.com>
2024-01-16 11:17:41 -0500

    Creacion Activity terminos y condiciones

--- app/src/main/java/com/app/running/LoginActivity.kt
+++ app/src/main/java/com/app/running/LoginActivity.kt
@@ -40,6 +40,21 @@ class LoginActivity : AppCompatActivity() {
     fun login(view: View){
         loginUser()
     }
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if(currentUser != null){
+            goHome(currentUser.email.toString(),currentUser.providerId)
+        }
+    }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+        val startMain = Intent(Intent.ACTION_MAIN)
+        startMain.addCategory(Intent.CATEGORY_HOME)
+        startMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(startMain)
+    }
     private fun loginUser() {
         email = etEmail.text.toString()
         password = etPassword.text.toString()
@@ -81,4 +96,9 @@ class LoginActivity : AppCompatActivity() {
                 else Toast.makeText(this, "Error, algo salió mal", Toast.LENGTH_SHORT).show()
             }
     }
+    fun goTerms(v: View){
+        val intent = Intent(this, TermsActivity::class.java)
+        startActivity(intent)
+    }
+    
 }
---
+++ app/src/main/java/com/app/running/TermsActivity.kt
@@ -0,0 +1,16 @@
+package com.app.running
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class TermsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms)
+
+//        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_terms)
+//        setSupportActionBar(toolbar)
+//        toolbar.title = getString(R.id.bar_title_terms)
+    }
+}
